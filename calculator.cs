7aca41e1a0a9bae477079c236d90f56f444b8418
krista7298 <kriagu7298@students.ecpi.edu>
2026-06-26 20:57:04 -0400

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,31 @@
+/*******************************************************************
+* Name: Krista Agustin
+* Date: 06/26/2026
+* Assignment SDC220 Calculator Project Phase #2
+*
+* Calculator class.
+* This class performs basic calculator operations.
+*/
+
+public class Calculator
+{
+    public double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public double Subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public double Multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    public double Divide(double num1, double num2)
+    {
+        return num1 / num2;
+    }
+}
